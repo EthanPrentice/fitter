662bf1702754b2f07ad4a84f410de69855cae046
napa13 <navitapatel19393@gmail.com>
2020-09-26 14:27:03 -0400

    Update deleteFsUser.ts

--- Firebase_Functions/functions/src/User/deleteFsUser.ts
+++ Firebase_Functions/functions/src/User/deleteFsUser.ts
@@ -4,17 +4,24 @@ const admin = require('firebase-admin');
 
 const db = admin.firestore();
 
-
+/*
+-> Listen to the Users Table for onDelete to be triggered
+-> Check to see if there are any subcollections initialized
+-> These subcollections would be the weight_logs, exercise_logs and user_preferences
+-> If they exist delete and await confirmation that they were deleted successfully
+*/
 
 // Delete user account in firestore
 export const deleteFsUser = functions.firestore.document('users/{userID}')
     .onDelete((snap, context) => {
     deleteCollection(db, 'users/{userID}/exercise_log', snap.size);
     deleteCollection(db, 'users/{userID}/weight_log', snap.size);
-    //deleteCollection(db, 'users/{userID}/user_preferences', snap.size);
+    deleteCollection(db, 'users/{userID}/user_preferences', snap.size);
 });
 
 
+//delete a collection (generic) function
+//calls a helper function to delete docs in batch
 async function deleteCollection(db, collectionPath, batchSize) {
   const collectionRef = db.collection(collectionPath);
   const query = collectionRef.orderBy('id').limit(batchSize);
@@ -24,6 +31,7 @@ async function deleteCollection(db, collectionPath, batchSize) {
   });
 }
 
+//helper function to delete docs in batch 
 async function deleteQueryBatch(db, query, resolve) {
   const snapshot = await query.get();
 
@@ -39,22 +47,5 @@ async function deleteQueryBatch(db, query, resolve) {
       batch.delete(doc.ref);
     });
     await batch.commit();
-
-    // Recurse on the next process tick, to avoid
-    // exploding the stack.
-    //process.nextTick(() => {
-    //  deleteQueryBatch(db, query, resolve);
-    //});
 }
 
-/*
-Second Function
-
--> Listen to the Users Table for onDelete to be triggered
--> Check to see if there are any subcollections initialized (They are stored under the UID of the user in another collection)
-    -> These DBs would be the weight_logs, exercise_logs and user_preferences
--> If they exist delete and await confirmation that they were deleted successfully
-
-
-
-*/
