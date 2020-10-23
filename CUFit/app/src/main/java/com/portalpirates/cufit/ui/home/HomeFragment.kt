244c60dd8e2d8c575a82b2045957862f60927bb3
Ethan Prentice <ethanp5262@gmail.com>
2020-10-23 16:44:27 -0400

    Cleaned up SwimlaneItemView code, and added swimlane support using these item views

--- CUFit/app/src/main/java/com/portalpirates/cufit/ui/home/HomeFragment.kt
+++ CUFit/app/src/main/java/com/portalpirates/cufit/ui/home/HomeFragment.kt
@@ -1,16 +1,53 @@
 package com.portalpirates.cufit.ui.home
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.portalpirates.cufit.R
+import com.portalpirates.cufit.datamodel.data.util.SwimlaneItem
 import com.portalpirates.cufit.ui.FitFragment
+import com.portalpirates.cufit.ui.view.swimlane.SwimlaneAdapter
+import com.portalpirates.cufit.ui.view.swimlane.SwimlaneView
 
 class HomeFragment : FitFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.home_frag_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        class SwimlaneItemTest(private val title: String) : SwimlaneItem {
+            override fun getTitle(): String {
+                return title
+            }
+            override fun getDrawable(): Drawable? {
+                return null
+            }
+        }
+
+        val swimlaneItems = ArrayList<SwimlaneItem>().apply {
+            add(SwimlaneItemTest("Test 1"))
+            add(SwimlaneItemTest("Test 2"))
+            add(SwimlaneItemTest("Test 3"))
+            add(SwimlaneItemTest("Test 4"))
+            add(SwimlaneItemTest("Test 5"))
+            add(SwimlaneItemTest("Test 6"))
+            add(SwimlaneItemTest("Test 7"))
+        }
+
+        fun onClickTest(v: View?, item: SwimlaneItem) {
+            Toast.makeText(requireContext(), "${item.getTitle()} was clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        val swimlaneAdapter = SwimlaneAdapter(requireContext(), swimlaneItems, ContextCompat.getDrawable(requireContext(), R.drawable.default_avatar)!!, ::onClickTest)
+        val swimlane = view.findViewById<SwimlaneView>(R.id.swimlane)
+        swimlane.adapter = swimlaneAdapter
     }
 
     override fun onResume() {
---
+++ CUFit/app/src/main/java/com/portalpirates/cufit/ui/view/swimlane/SwimlaneView.kt
@@ -0,0 +1,24 @@
+package com.portalpirates.cufit.ui.view.swimlane
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class SwimlaneView(context: Context, attrs: AttributeSet?, defStyle: Int) : RecyclerView(context, attrs, defStyle) {
+    constructor(context: Context) : this(context, null, 0)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    var visibleItems = 3.25f
+
+    init {
+        layoutManager = object : LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false) {
+            override fun checkLayoutParams(lp: LayoutParams?): Boolean {
+                // force width of viewHolder here
+                lp?.width = (width / visibleItems).toInt()
+                return true
+            }
+
+        }
+    }
+}
