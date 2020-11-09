a9d6cea21370a38e8c883ae01734765fdfe2cb02
Simon Roy-Halverson <28677261+simonroy-h@users.noreply.github.com>
2020-11-08 21:20:53 -0500

    CUF-67 Create a workout class

--- CUFit/datamodel/src/main/java/com/portalpirates/cufit/datamodel/data/workout/Workout.kt
+++ CUFit/datamodel/src/main/java/com/portalpirates/cufit/datamodel/data/workout/Workout.kt
@@ -1,12 +1,31 @@
 package com.portalpirates.cufit.datamodel.data.workout
 
 import android.graphics.drawable.Drawable
+import com.portalpirates.cufit.datamodel.data.user.FitUser
 import com.portalpirates.cufit.datamodel.data.util.SwimlaneItem
 
-class Workout : SwimlaneItem {
+open class Workout(
+        var name: String,
+        var description: String? = null,
+        var owner: FitUser,
+        var public: Boolean,
+        var subscribers: List<FitUser>? = null,
+        var exercises: List<Exercise>? = null,
+        var targetMuscleGroups: List<MuscleGroup>? = null
+) : SwimlaneItem {
+    // copy constructor
+    constructor(workout: Workout) : this(
+            workout.name,
+            workout.description,
+            workout.owner,
+            workout.public,
+            workout.subscribers,
+            workout.exercises,
+            workout.targetMuscleGroups
+    )
 
     override fun getTitle(): String {
-        return "Workout"
+        return name
     }
 
     override fun getDrawable(): Drawable? {
