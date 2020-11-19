4eaac5162b05e0f6c47f45918ab3b79e930d3c9b
Ethan Prentice <ethanp5262@gmail.com>
2020-11-18 22:04:32 -0500

    access modifier fix

--- CUFit/datamodel/src/main/java/com/portalpirates/cufit/datamodel/adt/Manager.kt
+++ CUFit/datamodel/src/main/java/com/portalpirates/cufit/datamodel/adt/Manager.kt
@@ -1,6 +1,6 @@
 package com.portalpirates.cufit.datamodel.adt
 
-abstract class Manager protected constructor() {
+abstract class Manager internal constructor() {
     internal val cloudInterfaces = HashMap<Int, CloudInterface>()
     internal val dataProcessors = HashMap<Int, DataProcessor>()
     protected val providers = HashMap<Int, Provider>()
