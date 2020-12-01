9eec071ed2e164a059b1062411ab070a5e260908
Simon Roy-Halverson <28677261+simonroy-h@users.noreply.github.com>
2020-12-01 14:00:07 -0500

    WorkoutReceiver class

--- CUFit/datamodel/src/main/java/com/portalpirates/cufit/datamodel/workout/receiver/WorkoutReceiver.kt
+++ CUFit/datamodel/src/main/java/com/portalpirates/cufit/datamodel/workout/receiver/WorkoutReceiver.kt
@@ -1,12 +1,23 @@
 package com.portalpirates.cufit.datamodel.workout.receiver
 
-import com.portalpirates.cufit.datamodel.adt.DataProcessor
 import com.portalpirates.cufit.datamodel.adt.Manager
 import com.portalpirates.cufit.datamodel.adt.Receiver
+import com.portalpirates.cufit.datamodel.adt.TaskListener
+import com.portalpirates.cufit.datamodel.data.workout.WorkoutBuilder
+import com.portalpirates.cufit.datamodel.data.workout.WorkoutField
 import com.portalpirates.cufit.datamodel.workout.WorkoutManager
+import com.portalpirates.cufit.datamodel.workout.processing.WorkoutManagementDataProcessor
 
 class WorkoutReceiver(manager: Manager) : Receiver(manager) {
 
-    override val dataProcessor: DataProcessor
-        get() = (manager as WorkoutManager).getDataProcessor()
+    override val dataProcessor: WorkoutManagementDataProcessor
+        get() = (manager as WorkoutManager).managementDataProcessor
+
+    fun createWorkout(builder: WorkoutBuilder, listener: TaskListener<Unit?>) {
+        dataProcessor.createWorkout(builder, listener)
+    }
+
+    fun updateWorkout(fields: HashMap<WorkoutField, Any?>, listener: TaskListener<Unit?>) {
+        dataProcessor.updateWorkout(fields, listener)
+    }
 }
