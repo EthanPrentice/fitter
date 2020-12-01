d0d1031e02126b8b6fb05760f950eaf98ef5cdb9
Simon Roy-Halverson <28677261+simonroy-h@users.noreply.github.com>
2020-12-01 12:04:46 -0500

    Exercise class

--- CUFit/datamodel/src/main/java/com/portalpirates/cufit/datamodel/data/workout/Exercise.kt
+++ CUFit/datamodel/src/main/java/com/portalpirates/cufit/datamodel/data/workout/Exercise.kt
@@ -2,5 +2,23 @@ package com.portalpirates.cufit.datamodel.data.workout
 
 import com.portalpirates.cufit.datamodel.data.measure.Weight
 
-class Exercise(var name: String, var weight: Weight?, var sets: Int, var reps: Int) {
+open class Exercise(
+        var name: String,
+        var description: String? = null,
+        var instructions: List<String>? = null,
+        var targetMuscleGroups: List<MuscleGroup>? = null,
+        var weight: Weight?,
+        var sets: Int,
+        var reps: Int
+) {
+    // copy constructor
+    constructor(exercise: Exercise) : this(
+            exercise.name,
+            exercise.description,
+            exercise.instructions,
+            exercise.targetMuscleGroups,
+            exercise.weight,
+            exercise.sets,
+            exercise.reps
+    )
 }
